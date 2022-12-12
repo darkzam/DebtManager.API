77fab56d8984b8e0a4e753059eceb63149fa76b5
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2022-12-11 23:35:22 -0500

    Refactored BaseEndpoint abstract class

--- src/DebtManager.API/Endpoints/Debt/GetDebt.cs
+++ src/DebtManager.API/Endpoints/Debt/GetDebt.cs
@@ -1,22 +1,14 @@
 ﻿using DebtManager.Application.Common.Interfaces;
 using DebtManager.Domain.Models;
 
-public class GetDebt : BaseEndpoint
+public class GetDebt : BaseEndpoint<Debt>
 {
-    private readonly WebApplication _webApplication;
-    public GetDebt(Uri baseRoute,
-                   WebApplication webApplication)
-    {
-        BasePath = baseRoute ?? throw new ArgumentNullException(nameof(baseRoute));
-        Action = new Uri(nameof(Debt), UriKind.Relative);
-        Route = new Uri($"{BasePath.ToString()}{Action.ToString()}", UriKind.Relative);
-        HttpVerb = HttpVerb.Get;
-        _webApplication = webApplication ?? throw new ArgumentNullException(nameof(webApplication));
-    }
+    public GetDebt(Uri baseRoute, WebApplication webApplication) : base(baseRoute, webApplication)
+    { }
 
     public override void Initialize()
     {
-        _webApplication.Map(Route.OriginalString, ProcessRequest);
+        WebApplication.MapGet(Route.OriginalString, ProcessRequest);
     }
 
     private async Task<IResult> ProcessRequest(IUnitOfWork unitOfWork)
