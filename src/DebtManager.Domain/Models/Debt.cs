186d95475233c9c2ea488f5c80d295837ab69624
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2022-12-13 14:34:23 -0500

    Added Initial Migration

--- src/DebtManager.Domain/Models/Debt.cs
+++ src/DebtManager.Domain/Models/Debt.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Code { get; set; }
-        public User Host { get; set; }
+        public User? Host { get; set; }
         public string Title { get; set; }
         public decimal Total { get; set; }
         public decimal ServiceRate { get; set; }
--- src/DebtManager.Domain/Models/DebtDetail.cs
+++ src/DebtManager.Domain/Models/DebtDetail.cs
@@ -3,8 +3,8 @@
     public class DebtDetail
     {
         public Guid Id { get; set; }
-        public Debt Debt { get; set; }
-        public Product Product { get; set; }
+        public Debt? Debt { get; set; }
+        public Product? Product { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
     }
