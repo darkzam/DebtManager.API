6025389668d5b58d50188b2c9e8cc069e8fbabff
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2023-03-10 23:48:01 -0500

    Added IpoconsumoTax Field migration. Added includes for GetAll to DebtRepository.

---
+++ src/DebtManager.Infrastructure/Migrations/20230311043625_Added_IpoconsumoTax_Field_To_Debt_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebtManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIpoconsumoTaxFieldToDebtTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IpoconsumoTax",
+                table: "Debt",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpoconsumoTax",
+                table: "Debt");
+        }
+    }
+}
