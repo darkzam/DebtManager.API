e9638999e7c5157bfd55078ac7101b7510a60732
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2023-03-30 18:31:29 -0500

    Refactored PostPaymentCollection endpoint to handle insertion by individual ChargeIds or charges by Usernames.

---
+++ src/DebtManager.API/Configuration/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DebtManager.API.Configuration.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool HasItems<T>(this IEnumerable<T> collection)
+        {
+            return collection?.Any() ?? false;
+        }
+    }
+}
--- src/DebtManager.API/Models/PostPaymentDto.cs
+++ src/DebtManager.API/Models/PostPaymentDto.cs
@@ -1,7 +1,57 @@
-﻿namespace DebtManager.API.Models
+﻿using DebtManager.API.Configuration.Extensions;
+
+namespace DebtManager.API.Models
 {
     public class PostPaymentDto
     {
-        public Guid ChargeId { get; set; }
+        public IEnumerable<Guid> ChargeIds { get; set; }
+        public IEnumerable<string> Usernames { get; set; }
+        public bool PreApprovedPayments { get; set; }
+
+        public ModelValidationResult ValidateModel()
+        {
+            if (!ChargeIds.HasItems() && !Usernames.HasItems())
+            {
+                return new ModelValidationResult
+                {
+                    Success = false,
+                    Message = $"{nameof(ChargeIds)} or {nameof(Usernames)} cannot be empty."
+                };
+            }
+
+            if (ChargeIds.HasItems())
+            {
+                var invalidGuids = ChargeIds.Where(x => x == Guid.Empty);
+
+                if (invalidGuids.Any())
+                {
+                    return new ModelValidationResult
+                    {
+                        Success = false,
+                        Message = $"{nameof(ChargeIds)}: Empty Guids are not allowed."
+                    };
+                }
+            }
+
+            if (Usernames.HasItems())
+            {
+                var invalidStrings = Usernames.Where(x => string.IsNullOrWhiteSpace(x));
+
+                if (invalidStrings.Any())
+                {
+                    return new ModelValidationResult
+                    {
+                        Success = false,
+                        Message = $"{nameof(Usernames)}: Empty Strings are not allowed."
+                    };
+                }
+            }
+
+            return new ModelValidationResult
+            {
+                Success = true,
+                Message = string.Empty
+            };
+        }
     }
 }
